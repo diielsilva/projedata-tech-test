87fb2b26bac2890f23d9c774ea4e7b7ca9673abf
Daniel Silva <diielsilvasousa@gmail.com>
2025-08-12 11:45:26 -0300

    fix: fixing birth date convertion

--- src/controller/FuncionarioController.java
+++ src/controller/FuncionarioController.java
@@ -5,6 +5,7 @@ import repository.FuncionarioRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Scanner;
 
 public class FuncionarioController {
@@ -20,23 +21,17 @@ public class FuncionarioController {
             System.out.print("Nome: ");
             String nome = entrada.next();
 
-            System.out.println();
-
             System.out.print("Data de nascimento (dd/mm/aaaa): ");
-            String dataDeNascimento = entrada.next();
-
-            System.out.println();
+            LocalDate dataDeNascimento = converterDataNascimentoParaUmFormatoValido(entrada.next());
 
             System.out.print("Salário (ex: 1512.50): ");
             double salario = entrada.nextDouble();
-
-            System.out.println();
 
             System.out.print("Função: ");
             String funcao = entrada.next();
 
             Funcionario funcionario =
-                    new Funcionario(nome, LocalDate.parse(dataDeNascimento), BigDecimal.valueOf(salario), funcao);
+                    new Funcionario(nome, dataDeNascimento, BigDecimal.valueOf(salario), funcao);
 
             funcionarioRepository.adicionar(funcionario);
 
@@ -44,5 +39,23 @@ public class FuncionarioController {
         } catch (Exception ex) {
             return "Os campos recebidos foram inválidos, tente novamente!";
         }
+    }
+
+    public void listarFuncionarios() {
+        List<Funcionario> funcionarios = funcionarioRepository.getFuncionarios();
+
+        for (Funcionario funcionario : funcionarios) {
+            System.out.println(funcionario);
+            System.out.println("----------");
+        }
+    }
+
+    private LocalDate converterDataNascimentoParaUmFormatoValido(String dataNascimento) {
+        String[] dataNascimentoSeparadaEntreDiaMesEAno = dataNascimento.split("/");
+        int dia = Integer.parseInt(dataNascimentoSeparadaEntreDiaMesEAno[0]);
+        int mes = Integer.parseInt(dataNascimentoSeparadaEntreDiaMesEAno[1]);
+        int ano = Integer.parseInt(dataNascimentoSeparadaEntreDiaMesEAno[2]);
+
+        return LocalDate.of(ano, mes, dia);
     }
 }
