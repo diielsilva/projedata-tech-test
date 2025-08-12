1a1b41a1e1ba6bb8eefcbc22391984579e7ec322
Daniel Silva <diielsilvasousa@gmail.com>
2025-08-12 16:34:54 -0300

    refact: splitting logic that executes the chosen option

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class MenuController {
     private final Scanner entrada = new Scanner(System.in);
+    private boolean estaExecutando = true;
     private final FuncionarioController funcionarioController;
+
 
     public MenuController(FuncionarioController funcionarioController) {
         this.funcionarioController = funcionarioController;
     }
 
-    public void exibirMenu() {
-        boolean oProgramaEstaExecutando = true;
-
+    public void iniciar() {
         do {
             try {
                 System.out.print("""
@@ -33,47 +33,51 @@ public class MenuController {
 
                 int opcao = entrada.nextInt();
 
-                switch (opcao) {
-                    case 0:
-                        oProgramaEstaExecutando = false;
-                        break;
-                    case 1:
-                        funcionarioController.adicionar();
-                        break;
-                    case 2:
-                        funcionarioController.listar();
-                        break;
-                    case 3:
-                        funcionarioController.aniversariantesDoMes();
-                        break;
-                    case 4:
-                        funcionarioController.obterMaisExperiente();
-                        break;
-                    case 5:
-                        funcionarioController.obterAgrupadosPorFuncao();
-                        break;
-                    case 6:
-                        funcionarioController.obterOrdenadosPorNome();
-                        break;
-                    case 7:
-                        funcionarioController.obterTotalDosSalarios();
-                        break;
-                    case 8:
-                        funcionarioController.removerPorNome();
-                        break;
-                    case 9:
-                        funcionarioController.darAumentoATodos();
-                        break;
-                    default:
-                        System.out.println("Opção inválida!");
-                        break;
-                }
+                executarOpcaoEscolhida(opcao);
 
             } catch (Exception exception) {
                 System.out.println("Opção inválida!");
                 entrada.nextLine();
             }
 
-        } while (oProgramaEstaExecutando);
+        } while (estaExecutando);
+    }
+
+    private void executarOpcaoEscolhida(int opcao) {
+        switch (opcao) {
+            case 0:
+                estaExecutando = false;
+                break;
+            case 1:
+                funcionarioController.adicionar();
+                break;
+            case 2:
+                funcionarioController.listar();
+                break;
+            case 3:
+                funcionarioController.aniversariantesDoMes();
+                break;
+            case 4:
+                funcionarioController.obterMaisExperiente();
+                break;
+            case 5:
+                funcionarioController.obterAgrupadosPorFuncao();
+                break;
+            case 6:
+                funcionarioController.obterOrdenadosPorNome();
+                break;
+            case 7:
+                funcionarioController.obterTotalDosSalarios();
+                break;
+            case 8:
+                funcionarioController.removerPorNome();
+                break;
+            case 9:
+                funcionarioController.darAumentoATodos();
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
     }
 }
